4821f6989a255f408068d43fc5c0dd722674a244
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-03 05:04:46 +0100

    Update git.js
    more cleanups.

--- git.js
+++ git.js
@@ -8,16 +8,18 @@ const toExecPromise = async (cmd, timeout=60000) => new Promise((resolve) => {
   exec(cmd, (err, stdout) => clearTimeout(id) && ((err) ? error(err) : resolve(stdout));
 });
 
-export const moduleExports = {
-  git: (cmd) => toExecPromise(`git ${cmd}`),
-  getCurrentBranch: async () => (await git(`rev-parse --abbrev-ref HEAD`)).trim(),
-  getConfigData: async (configItem) => await git(configItem).catch((_)=>""),
-  getBranches: async () => Array.from((await git("branch --no-color"))
+const git = (cmd) => toExecPromise(`git ${cmd}`);
+
+const getBranches = async () => Array.from((await git("branch --no-color"))
     .split("\n").map((branch) => branch.replace("*", "").trim())
-    .reduce((collector, branch) => collector.add(branch), new Set())),
-  hasBranch: async (branch) => (await git.getBranches()).includes(branch),
-  switchBranch: async (branch) => (await git.hasBranch(branch)) 
-    ? await git(`checkout -b ${branch}`)
-    : await git(`checkout ${branch}`),
+    .reduce((collector, branch) => collector.add(branch), new Set()));
+
+const hasBranch: async (branch) => (await getBranches()).includes(branch);
+
+export const moduleExports = {
+  git, getCurrentBranch: async () => (await git(`rev-parse --abbrev-ref HEAD`)).trim(),
+  getConfigData: async (configItem) => await git(configItem).catch((_)=>""),
+  getBranches, hasBranch, switchBranch: async (branch) => (await hasBranch(branch)) 
+    ? await git(`checkout -b ${branch}`) : await git(`checkout ${branch}`),
   getRepoName: async () => path.basename((await git("rev-parse --show-toplevel"))).trim()
 };
