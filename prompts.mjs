fc2fe90559aa733bc08c308f727d9277bc1cd024
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-03 05:55:17 +0100

    Update prompts.mjs
    gave it some more love

--- prompts.mjs
+++ prompts.mjs
@@ -9,7 +9,7 @@ const upperCase = async (word) => Promise.resolve(word.charAt(0).toUpperCase() +
   .catch((err) => { throw new TypeError(`Expected string got ${typeof word}: ${err}`); });
 
 export const askQuestion = (promptOps) => new Promise((resolve, reject) =>
-      prompt.get(promptOps, (cancel, res) => (cancel) 
+      prompt.get(Object.assign({ type: "string", before: (value) => value.trim() }, promptOps), (cancel, res) => (cancel) 
           ? reject(new Error(" 🙅 User canceled."));
           : resolve(res.question)));
 
@@ -17,65 +17,38 @@ export const Prompts = {
   askQuestion, askRepoName: () => askQuestion({
     description: "Name of Git repository:",
     default: path.basename(process.cwd()),
-    type: "string", before: (value) => value.trim(),
   }),
   askProjectName: (repo) => askQuestion({
     description: "Name of project:",
     default: `The ${upperCase(repo)} API`,
-    type: "string", before: (value) => value.trim(),
   }),
   askUserName: async () => askQuestion({
     description: "Name of Primary Editor of the spec:",
     default: (await getConfigData("config user.name")).trim(),
-    type: "string", before: (value) => value.trim(),
   }),
-  askAffiliation(hint = "") {
-    const promptOps = {
-      description: `Company affiliation(e.g., ${
-        upperCase(hint) || "Monsters"
-      } Inc.):`,
-      default: upperCase(hint),
-      type: "string", before: (value) => value.trim(),
-    };
-    return this.askQuestion(promptOps);
-  },
-  askAffiliationURL(emailHint = "") {
-    const [, hint] = emailHint.match(/(?:@)(.+)/);
-    const promptOps = {
-      description: "Company URL:",
-      type: "string", before: (value) => value.trim(),
-    };
-    if (hint) {
-      promptOps.default = `https://${hint}`;
-    }
-    return this.askQuestion(promptOps);
-  },
-  async askEmail() {
-    const email = await getConfigData("config user.email");
-    const promptOps = {
+  askAffiliation: (hint = "") => askQuestion({
+    description: `Company affiliation(e.g., ${
+      upperCase(hint) || "Monsters"
+    } Inc.):`,
+    default: upperCase(hint),
+  }),
+  askAffiliationURL: (emailHint = "") => (emailHint = emailHint.match(/(?:@)(.+)/)[1] || '') && askQuestion({
+    description: "Company URL:",
+    default: (emailHint) ? `https://${emailHint}` : undefined,
+  }),
+  askEmail: async () => askQuestion({
       description: "Email (optional):",
-      default: email.trim(),
-      type: "string", before: (value) => value.trim(),
-    };
-    return this.askQuestion(promptOps);
-  },
-  askWhichGitBranch() {
-    const promptOps = {
-      description: "Main git branch for the spec:",
-      default: "main",
-      pattern: /^[\w\-]+$/,
-      message: "Name must be only letters and dashes",
-      type: "string", before: (value) => value.trim(),
-    };
-    return this.askQuestion(promptOps);
-  },
-  askWhichPreProcessor() {
-    const promptOps = {
-      description: "Spec preprocessor (respec or bikeshed):",
-      default: "respec",
-      pattern: /^(respec|bikeshed|bs)$/i,
-      type: "string", before: (value) => value.trim(),
-    };
-    return this.askQuestion(promptOps);
-  },
+      default: (await getConfigData("config user.email")).trim(),
+  }),
+  askWhichGitBranch: () => askQuestion({
+    description: "Main git branch for the spec:",
+    default: "main",
+    pattern: /^[\w\-]+$/,
+    message: "Name must be only letters and dashes",
+  }),
+  askWhichPreProcessor: () => askQuestion({
+    description: "Spec preprocessor (respec or bikeshed):",
+    default: "respec",
+    pattern: /^(respec|bikeshed|bs)$/i,
+  }),
 };
