783e9e3388653780b0e8abe9f101eb7cc7641ffb
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-03 06:22:05 +0100

    Update and rename theme.mjs to chalk.js

--- chalk.js
+++ chalk.js
@@ -5,6 +5,4 @@ export const gr = chalk.default.gray.bind(chalk);
 export const m = chalk.default.magenta.bind(chalk);
 export const r = chalk.default.red.bind(chalk);
 export const y = chalk.default.yellow.bind(chalk);
-export const heading = function (text) {
-  return chalk.default.underline(`\n${text.toUpperCase()}\n`);
-};
+export const heading = (text) => chalk.default.underline(`\n${text.toUpperCase()}\n`);
