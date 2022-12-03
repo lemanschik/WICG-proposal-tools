d4c94b14017292899578a40eb18c729e4ded3359
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-03 05:36:04 +0100

    Update prompts.mjs
    Little cleanups but needs more love

--- prompts.mjs
+++ prompts.mjs
@@ -1,79 +1,41 @@
-"use strict";
-import { git } from "./git.mjs";
-import path from "path";
-import prompt from "prompt";
+import { getConfigData } from "./git.js";
+import path from "node:path";
+import prompt from "node:prompt";
 
-// Configure prompt
 prompt.message = " 👉 ";
 prompt.delimiter = "";
 
-// Utility function to convert first letter to uppercase.
-function upperCaseFirstLetter(word) {
-  if (typeof word !== "string") {
-    throw new TypeError("Expected string");
-  }
-  if (!word) {
-    return word;
-  }
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+const upperCase = async (word) => Promise.resolve(word.charAt(0).toUpperCase() + word.slice(1))
+  .catch((err) => { throw new TypeError(`Expected string got ${typeof word}: ${err}`); });
 
-// User prompt tasks
+export const askQuestion = (promptOps) => new Promise((resolve, reject) =>
+      prompt.get(promptOps, (cancel, res) => (cancel) 
+          ? reject(new Error(" 🙅 User canceled."));
+          : resolve(res.question)));
+
 export const Prompts = {
-  askQuestion(promptOps) {
-    return new Promise((resolve, reject) => {
-      prompt.get(promptOps, (err, res) => {
-        if (err) {
-          return reject(new Error(" 🙅 User canceled."));
-        }
-        resolve(res.question);
-      });
-    });
-  },
-  askRepoName() {
-    const promptOps = {
-      description: "Name of Git repository:",
-      default: path.basename(process.cwd()),
-      type: "string",
-      before(value) {
-        return value.trim();
-      },
-    };
-    return this.askQuestion(promptOps);
-  },
-  askProjectName(repo) {
-    const promptOps = {
-      description: "Name of project:",
-      default: `The ${upperCaseFirstLetter(repo)} API`,
-      type: "string",
-      before(value) {
-        return value.trim();
-      },
-    };
-    return this.askQuestion(promptOps);
-  },
-  async askUserName() {
-    const user = await git.getConfigData("config user.name");
-    const promptOps = {
-      description: "Name of Primary Editor of the spec:",
-      default: user.trim(),
-      type: "string",
-      before(value) {
-        return value.trim();
-      },
-    };
-    return this.askQuestion(promptOps);
-  },
+  askQuestion, askRepoName: () => askQuestion({
+    description: "Name of Git repository:",
+    default: path.basename(process.cwd()),
+    type: "string", before: (value) => value.trim(),
+  }),
+  askProjectName: (repo) => askQuestion({
+    description: "Name of project:",
+    default: `The ${upperCase(repo)} API`,
+    type: "string", before: (value) => value.trim(),
+  }),
+  askUserName: async () => askQuestion({
+    description: "Name of Primary Editor of the spec:",
+    default: (await getConfigData("config user.name")).trim(),
+    type: "string", before: (value) => value.trim(),
+  }),
   askAffiliation(hint = "") {
     const promptOps = {
       description: `Company affiliation(e.g., ${
-        upperCaseFirstLetter(hint) || "Monsters"
+        upperCase(hint) || "Monsters"
       } Inc.):`,
-      default: upperCaseFirstLetter(hint),
-      type: "string",
-      before(value) {
-        return value.trim();
-      },
+      default: upperCase(hint),
+      type: "string", before: (value) => value.trim(),
     };
     return this.askQuestion(promptOps);
   },
@@ -81,10 +43,7 @@ export const Prompts = {
     const [, hint] = emailHint.match(/(?:@)(.+)/);
     const promptOps = {
       description: "Company URL:",
-      type: "string",
-      before(value) {
-        return value.trim();
-      },
+      type: "string", before: (value) => value.trim(),
     };
     if (hint) {
       promptOps.default = `https://${hint}`;
@@ -92,11 +51,11 @@ export const Prompts = {
     return this.askQuestion(promptOps);
   },
   async askEmail() {
-    const email = await git.getConfigData("config user.email");
+    const email = await getConfigData("config user.email");
     const promptOps = {
       description: "Email (optional):",
       default: email.trim(),
-      type: "string",
+      type: "string", before: (value) => value.trim(),
     };
     return this.askQuestion(promptOps);
   },
@@ -105,11 +64,8 @@ export const Prompts = {
       description: "Main git branch for the spec:",
       default: "main",
       pattern: /^[\w\-]+$/,
-      type: "string",
       message: "Name must be only letters and dashes",
-      before(value) {
-        return value.trim();
-      },
+      type: "string", before: (value) => value.trim(),
     };
     return this.askQuestion(promptOps);
   },
@@ -117,11 +73,8 @@ export const Prompts = {
     const promptOps = {
       description: "Spec preprocessor (respec or bikeshed):",
       default: "respec",
-      type: "string",
       pattern: /^(respec|bikeshed|bs)$/i,
-      before(value) {
-        return value.trim().toLowerCase();
-      },
+      type: "string", before: (value) => value.trim(),
     };
     return this.askQuestion(promptOps);
   },
